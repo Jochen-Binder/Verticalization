f4dd238d295e3c6fefefd86be8d4bc386f218097
Jochen-Binder <jochen.d.binder@gmail.com>
2019-01-09 11:43:10 +0100

    Included separator for variable names
    Used to be hard coded for '_'. Now any separator can be defined.

--- Verticalize.py
+++ Verticalize.py
@@ -12,26 +12,14 @@ class verticalize(object):
     def __init__(self, df):
         """
         Initialize the verticalize class
-        original: numpy darray matrix or dataframe to perform the ipfn on.
-        aggregates: list of numpy array or darray or pandas dataframe/series. The aggregates are the same as the marginals.
-        They are the target values that we want along one or several axis when aggregating along one or several axes.
-        dimensions: list of lists with integers if working with numpy objects, or column names if working with pandas objects.
-        Preserved dimensions along which we sum to get the corresponding aggregates.
-        convergence_rate: if there are many aggregates/marginal, it could be useful to loosen the convergence criterion.
-        max_iteration: Integer. Maximum number of iterations allowed.
-        verbose: integer 0, 1 or 2. Each case number includes the outputs of the previous case numbers.
-        0: Updated matrix returned. 
-        1: Flag with the output status (0 for failure and 1 for success).
-        2: dataframe with iteration numbers and convergence rate information at all steps. 
-        rate_tolerance: float value. If above 0.0, like 0.001, the algorithm will stop once the difference between the conv_rate variable of 2 consecutive iterations is below that specified value
-        For examples, please open the ipfn script or look for help on functions ipfn_np and ipfn_df
+        
         """
         self.original = df
         self.original['tmp_ID'] = range(len(self.original))
         self.cols = df.columns
 
         
-    def melt(self, df, columns):
+    def melt(self, df, columns, separator = "_"):
         melt_cols = columns 
         cols = self.cols
         diff = cols.difference(melt_cols)
@@ -47,10 +35,10 @@ class verticalize(object):
         del(ordered_cols, melt_cols, diff, cols)
     
         # Create 'brand' and 'item' variable and strip unnecessary strings
-        df['brand'] = df['variable_original'].apply(lambda x: x.split(sep="_")[1])
-        df['variable'] = df['variable_original'].map(lambda x: x.split(sep="_")[0] 
-                                                        if x.count('_')<=1 
-                                                        else x.split(sep="_")[0]+'_'+x.split(sep="_")[2])
+        df['brand'] = df['variable_original'].apply(lambda x: x.split(sep=separator)[1])
+        df['variable'] = df['variable_original'].map(lambda x: x.split(sep=separator)[0] 
+                                                        if x.count(separator)<=1 
+                                                        else x.split(sep=separator)[0]+'_'+x.split(sep=separator)[2])
         del df['variable_original']
         
         return df
